8c34d59a452a2cd090be6afc656583ff8ce560b0
federico.pupo <federico.pupo@promo.it>
2018-06-14 16:19:44 +0200

    no message

--- pronostici.php
+++ pronostici.php
@@ -1,11 +1,28 @@
 <?
 mysql_select_db("fantamondiale", mysql_connect("localhost", "root", ""));
 
-
+if(!empty($_POST)){
+	if($_POST['utente'] > 0){
+		mysql_query("DELETE FROM pronostici_partite WHERE utente = '".$_POST['utente']."'");
+		foreach($_POST['partite'] as $partita=>$pronostici){
+			mysql_query("INSERT INTO `pronostici_partite`(`partita`, `u_o`, `1x2`, `goal_1`, `goal_2`, `utente`) 
+				VALUES ('".$partita."','".$pronostici['u_o']."','".$pronostici['1x2']."','".$pronostici['goal_1']."','".$pronostici['goal_2']."','".$_POST['utente']."')");	
+		}
+	}
+}
 $squadre = array();
 $q = mysql_query("SELECT * FROM squadre"); 
 while($r = mysql_fetch_assoc($q)){
 	$squadre[$r['ID']] = $r['nome'];
+}
+
+
+$pronostici = array();
+if($_GET['utente'] > 0){
+	$q = mysql_query("SELECT * FROM pronostici_partite WHERE utente = '".$_GET['utente']."' ");
+	while($r = mysql_fetch_assoc($q)){
+		$pronostici[$r['ID']] = $r;
+	}
 }
 ?>
 <style>
@@ -27,50 +44,64 @@ tr td{
 	border-bottom:1px solid #999;
 }
 </style>
-<table cellpadding="0" cellspacing="0">
-	<tr>
-		<th colspan="10" align="left">Utente:
-			<select>
-				<option>Seleziona</option>
-				<?
-				$q = mysql_query("SELECT * FROM utenti ORDER BY nome");
-				while($r = mysql_fetch_assoc($q)){
-					?>
-					<option value="<?=$r['ID']?>"><?=$r['nome']?></option>
-					<?
-				}
-				?>
-			</select>
-		</th>
-	</tr>
-	<tr>
-		<th>ID</th>
-		<th>Data</th>
-		<th colspan="3">Partita</th>
-		<th colspan="3">Risultato</th>
-		<th>U/O</th>
-		<th>1X2</th>
-	</tr>
-	<?
-	$q = mysql_query("SELECT * FROM partite ORDER BY `datetime`");
-	while($r = mysql_fetch_assoc($q)){
-		?>
+<form action="" method="GET">
+	<table cellpadding="0" cellspacing="0">
 		<tr>
-			<td><?=$r['ID']?></td>
-			<td><?=formatta_data($r['datetime'])?></td>
-			<td><?=$squadre[$r['squadra_1']]?></td>
-			<td>-</td>
-			<td><?=$squadre[$r['squadra_2']]?></td>			
-			<td><input type="text" name="goal_2" style="width:50px"></td>
-			<td>-</td>
-			<td><input type="text" name="goal_1" style="width:50px"></td>
-			<td><input type="text" name="u_o" style="width:50px"></td>		
-			<td><input type="text" name="1x2" style="width:50px"></td>		
+			<th colspan="11" align="left">Utente:
+				<select name="utente" onchange="this.form.submit()">
+					<option value="0">Seleziona</option>
+					<?
+					$q = mysql_query("SELECT * FROM utenti ORDER BY nome");
+					while($r = mysql_fetch_assoc($q)){
+						?>
+						<option value="<?=$r['ID']?>"><?=$r['nome']?></option>
+						<?
+					}
+					?>
+				</select>
+			</th>
+		</tr>
+	</table>
+</form>
+<form action="" method="POST">
+	<input type="hidden" name="utente" value="<?=$_GET['utente']?>">
+	<table cellpadding="0" cellspacing="0">
+		<tr>
+			<th>ID</th>
+			<th>Data</th>
+			<th>Luogo</th>
+			<th colspan="3">Partita</th>
+			<th colspan="3">Risultato</th>
+			<th>U/O</th>
+			<th>1X2</th>
 		</tr>
 		<?
-	}
-	?>
-</table>
+		$q = mysql_query("SELECT * FROM partite ORDER BY `datetime`");
+		while($r = mysql_fetch_assoc($q)){
+			?>
+			<tr>
+				<td><?=$r['ID']?></td>
+				<td><?=formatta_data($r['datetime'])?></td>
+				<td><?=$r['location']?></td>
+				<td><?=$squadre[$r['squadra_1']]?></td>
+				<td>-</td>
+				<td><?=$squadre[$r['squadra_2']]?></td>			
+				<td><input type="text" name="partite[<?=$r['ID']?>][goal_1]" value="<?=$pronostici[$r['ID']]['goal_1']?>" style="width:50px"></td>
+				<td>-</td>
+				<td><input type="text" name="partite[<?=$r['ID']?>][goal_2]" value="<?=$pronostici[$r['ID']]['goal_2']?>" style="width:50px"></td>
+				<td><input type="text" name="partite[<?=$r['ID']?>][u_o]" value="<?=$pronostici[$r['ID']]['u_o']?>" style="width:50px"></td>		
+				<td><input type="text" name="partite[<?=$r['ID']?>][1x2]" value="<?=$pronostici[$r['ID']]['1x2']?>" style="width:50px"></td>		
+			</tr>
+			<?
+		}
+		?>
+		<tr>
+			<th colspan="11">
+				<input type="submit" value="Salva pronostici">
+			</th>
+		</tr>
+	</table>
+</form>
 <?
 
 function formatta_data($data){
